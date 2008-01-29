9818bf6f94a532767b9f998081c3b55a6b98e641
clark_upb <clark_upb@6764b5e5-2b31-0410-876a-b68d73e65804>
2008-01-29 20:02:59 +0000

    Oops...made a mistake
    git-svn-id: https://textmb.svn.sourceforge.net/svnroot/textmb/trunk@143 6764b5e5-2b31-0410-876a-b68d73e65804

--- config.php
+++ config.php
@@ -1,7 +1,4 @@
 <?php
-define('INSTALLATION_MODE', false, true);
-define('UPB_VERSION', '2.1.1b from svn trunk with additional changes', true);
-define('DB_DIR', './data_470104bf071657.22625643', true);
-define('ADMIN_EMAIL', 'clark@myupb.com', true);
-
+define('INSTALLATION_MODE', true, true);
+define('UPB_VERSION', '2.1.1b', true);
 ?>
