109f8bd1e399a0e8fe59c5d222b4fe5019bebb79
clark_upb <clark_upb@6764b5e5-2b31-0410-876a-b68d73e65804>
2008-02-04 17:27:18 +0000

    minor error fixed
    git-svn-id: https://textmb.svn.sourceforge.net/svnroot/textmb/trunk@146 6764b5e5-2b31-0410-876a-b68d73e65804

--- config.php
+++ config.php
@@ -1,18 +1,4 @@
 <?php
-
-
-
-define('INSTALLATION_MODE', false, true);
-
-
+define('INSTALLATION_MODE', true, true);
 define('UPB_VERSION', '2.1.1b', true);
-
-
-
-define('DB_DIR', './data_479fc281af7a19.26440894', true);
-
-
-
-define('ADMIN_EMAIL', 'clark@myupb.com', true);
-
 ?>
