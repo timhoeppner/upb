5b23e6df85c66fb7b43a4b4fb837b04a98a2f216
Rebles <Rebles@6764b5e5-2b31-0410-876a-b68d73e65804>
2008-03-09 19:05:33 +0000

    upb.initialize.php: forgot to include func.class.php
    git-svn-id: https://textmb.svn.sourceforge.net/svnroot/textmb/trunk@191 6764b5e5-2b31-0410-876a-b68d73e65804

--- includes/upb.initialize.php
+++ includes/upb.initialize.php
@@ -1,5 +1,5 @@
 <?php
-
+if(basename($_SERVER['PHP_SELF']) == 'upb.initialize.php') die('This is a wrapper script!');
 //Start session for all upb pages
 session_start();
 
@@ -29,6 +29,7 @@ if(!empty($GLOBALS['_ENV'])) foreach($GLOBALS["_REQUEST"] as $varname => $varval
 
 require_once("./includes/class/error.class.php");
 require_once("./config.php");
+require_once("./includes/class/func.class.php");
 require_once("./includes/class/tdb.class.php");
 require_once("./includes/class/config.class.php");
 require_once("./includes/class/mod_avatar.class.php");
