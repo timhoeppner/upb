cba4915ca78d36d90795231947e51583c381b45f
clark_upb <clark_upb@6764b5e5-2b31-0410-876a-b68d73e65804>
2009-03-14 02:12:44 +0000

    git-svn-id: https://textmb.svn.sourceforge.net/svnroot/textmb/trunk@358 6764b5e5-2b31-0410-876a-b68d73e65804

--- downloadattachment.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once("./includes/upb.initialize.php");
-	require_once("./includes/class/upload.class.php");
-    $upload = new upload(DB_DIR, 0,$_CONFIG['fileupload_location']);
-    $upload->getFile((int) $_GET["id"]);
-	// Update the download count
-	$upload->edit("uploads", (int) $_GET["id"], array("downloads" => ((int)$upload->file["downloads"] + 1)));
-	$upload->dumpFile();
-?>
