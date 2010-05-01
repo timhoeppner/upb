0e1446a568ca79ba01b3c87534c08826e8cda08f
clark_upb <clark_upb@6764b5e5-2b31-0410-876a-b68d73e65804>
2010-05-01 14:40:22 +0000

    downloadattachment.php: Stopped unauthorised people from downloading files directly
    git-svn-id: https://textmb.svn.sourceforge.net/svnroot/textmb/trunk@428 6764b5e5-2b31-0410-876a-b68d73e65804

--- downloadattachment.php
+++ downloadattachment.php
@@ -1,9 +1,15 @@
 <?php
 	require_once("./includes/upb.initialize.php");
 	require_once("./includes/class/upload.class.php");
+    if (!($tdb->is_logged_in())) {
+           die("You do not have permission to download this file");
+    }
+    else
+    {
     $upload = new upload(DB_DIR, 0,$_CONFIG['fileupload_location']);
     $upload->getFile((int) $_GET["id"]);
 	// Update the download count
 	$upload->edit("uploads", (int) $_GET["id"], array("downloads" => ((int)$upload->file["downloads"] + 1)));
 	$upload->dumpFile();
+}
 ?>
