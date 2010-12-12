40ab91a993256c64070628927b050552d35f50bc
Clark <online@chris-kent.co.uk>
2010-12-12 14:54:13 +0000

    BugsId: 3124214

--- downloadattachment.php
+++ downloadattachment.php
@@ -6,10 +6,12 @@ if (!($tdb->is_logged_in())) {
 }
 else
 {
-	$upload = new upload(DB_DIR, 0,$_CONFIG['fileupload_location']);
-	$upload->getFile((int) $_GET["id"]);
+	if (FALSE === ($fRec = $tdb->get("forums", $_GET["id"]))) exitPage("Download topic does not exist.", true);
+	if ((int)$_COOKIE["power_env"] < $fRec[0]["view"]) exitPage("You do not have permission to download this file");
+  $upload = new upload(DB_DIR, 0,$_CONFIG['fileupload_location']);
+	$upload->getFile((int) $_GET["upload_id"]);
 	// Update the download count
-	$upload->edit("uploads", (int) $_GET["id"], array("downloads" => ((int)$upload->file["downloads"] + 1)));
+	$upload->edit("uploads", (int) $_GET["upload_id"], array("downloads" => ((int)$upload->file["downloads"] + 1)));
 	$upload->dumpFile();
 }
 ?>
